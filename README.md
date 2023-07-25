b9b17ad071c905cb0443e8ad9922309b02236222
Bird <45112815+aubirdgine@users.noreply.github.com>
2023-07-25 13:59:36 -0500

    Added origin check, og: tags, and supertag logic

--- README.md
+++ README.md
@@ -2,6 +2,58 @@
 
 Clips current webpage and selection to tana-paste format
 
+
+# How It Works
+
+The utility grabs the following information for the current tab:
+- Page Title
+- URL
+- Meta tags
+  - og:description
+  - og:title
+  - og:url
+  - og:type
+  - og:image
+  - og:site_name
+- <any selected text>
+
+The utility then checks the URL (<href.origin>) to see if the current tab is the root of the site or not. This allows multiple 'web pages' to be tied to a single root 'website' using a Part Of relationship in Tana.
+
+It also determines the 'type' of page by checking the og:type tag and parsing the URL for a filename or extension, then sets one of the following supertags for the node:
+- #article
+- #book
+- #video
+- #document (anything with an extension, other than .html)
+- #web-page (default)
+- #website (only for the origin site)
+
+Then the information is formatted as a Tana Paste entry and copied to the clipboard.
+
+# Formatting
+
+The basic Tana Paste data copied to the clipboard is the following:
+
+```
+%%tana%%
+- Origin site title #website
+  - Title:: <og:site_name or 'Website at href.origin.hostname'>
+  - URL:: <href.origin>
+  - Hostname:: <href.origin.hostname>
+
+
+- Title of current tab #<supertag>
+  - Title:: <of current tab>
+  - URL:: <of current tab>
+  - Description:: <from site metadata>
+  - Part Of Website:: [[<Tana reference to origin node above>]]
+  - Highlights
+    - <any selected text>
+    - <line breaks are preserved for multiple lines>
+```
+
+For posterity's sake, if the original meta tag differs from the cleaned up version, both will be included on their own lines. (i.e. - Description:: <cleaned up description> and - og:Description:: <original unchanged meta tag> may both be listed so you can compare them.)
+
+
 # Installing without building
 If you just want to use this extension without bothering with the source code and all that jazz, you can download the latest `release.zip` file from the `Releases` area (right hand side of the github project page). Then, go to the Chrome extension manager (`chrome://extensions`) and turn on `Developer mode`. This will give you access to the `Load unpacked` button and you can pick the downloaded zip file.
 
@@ -57,3 +109,10 @@ For CLI instructions see [User Guide &rarr;](https://oss.mobilefirst.me/extensio
 - [Publishing for Edge](https://docs.microsoft.com/en-us/microsoft-edge/extensions-chromium/publish/publish-extension)
 - [Publishing for Opera addons](https://dev.opera.com/extensions/publishing-guidelines/)
 - [Publishing for Firefox](https://extensionworkshop.com/documentation/publish/submitting-an-add-on/)
+
+
+# JB Notes To Self:
+- Build using `npm run build` for Chrome
+- Go to Chrome extension manager and `load unpacked` the `dist` directory to update extension
+- Run `xcrun safari-web-extension-converter <path to extension>` in terminal to convert for Safari (or manually copy changes to previously converted XCode project)
+- Build XCode project
--- src/clip.js
+++ src/clip.js
@@ -9,60 +9,92 @@ export function clipHTML(html) {
   // ref: Stackoverflow 
   window.focus();
   // grab the basic info from the page
-  const title = document.title;
-  const url = window.location.href;
-  let description = "";
-
-  // basic format of a tana-paste entry
-  let data = `%%tana%%\n- ${title} #website`;
-
+  let pgTitle = document.title;
+  let pgURL = new URL(window.location.href);
+  let pgDescription = '';
+  let pgPath = pgURL.pathname;
+  // boolean to indicate if we are at the root of the site
+  let is_root = pgPath === '/';
+  // default tana supertag is 'web-page'
+  let supertag = 'web-page';
+  // add a tana paste row for highlights just in case
+  let highlights = ['\n  - Highlights'];
+  // initialize the tana paste data
+  let siteName = '';
+  let tanaPasteData = '';
   let fields = [];
-  fields.push(`\n  - Url:: ${url}`);
+
+  // replace underscores and dashes with spaces in a filename string
+  const cleanFileName = (str) => str.replace(/_/g, ' ').replace(/-/g, ' ');
+  // convert a string to title case
+  const titleCase = (str) => str.replace(/\b\S/g, (t) => t.toUpperCase());
 
   const metaTags = document.querySelectorAll("meta");
 
   for (const element of metaTags) {
     if (element.name === "description") {
-      description = element.content;
-      fields.push("\n  - Description:: " + description);
+      pgDescription = element.content.trim();
+      if (pgDescription.length > 0) {
+        fields.push('\n  - Description:: ' + pgDescription);
+      }
     } else {
-      let property = element.getAttribute("property");
-      let content = element.content;
+      let property = element.getAttribute('property');
+      let content = element.content.trim();
       if (property === "og:description") {
-        // no point in duplicating the description
-        if (content != description) {
-          fields.push("\n  - og:Description:: " + content);
+        // no point in duplicating the description, but use it if we don't have one
+        if (pgDescription.length === 0) {
+          fields.push('\n  - Description:: ' + content);
+        } else if (content != pgDescription) {
+          fields.push('\n  - og:Description:: ' + content);
         }
       }
       if (property === "og:title") {
-        // no point in duplicating the title
-        if (content !== title) {
+        // no point in duplicating the title, but use it if we don't have one
+        if (pgTitle.length === 0) {
+          fields.push('\n  - Title:: ' + content);
+        } else if (content !== pgTitle) {
           fields.push("\n  - og:Title:: " + content);
         }
       }
       if (property === "og:url") {
         // no point in duplicating the url
-        if (content != url) {
+        if (content != pgURL.href) {
           fields.push("\n  - og:Url:: " + content);
         }
       }
       if (property === "og:type") {
-        fields.push("\n  - og:Type:: " + content);
+        switch (content) {
+          case 'article':
+            supertag = 'article';
+            break;
+          case 'book':
+            supertag = 'book';
+            break;
+          case 'video.other':
+            supertag = 'video';
+            break;
+          case 'video.movie':
+            supertag = 'video';
+            break;
+          case 'website':
+            supertag = 'web-page';
+            break;
+          default:
+            supertag = 'web-page';
+            fields.push('\n  - og:Type:: ' + content);
+            break;
+        }
       }
       if (property === "og:image") {
         fields.push("\n  - og:Image:: " + content);
       }
 
       if (property === "og:site_name") {
-        fields.push("\n  - og:Site:: " + content);
+        siteName = content;
       }
     }
   }
 
-  fields.forEach((field) => {
-    console.log(field);
-    data += field;
-  });
 
   // do we have selected text as well?
   if (html) {
@@ -99,13 +131,71 @@ export function clipHTML(html) {
       if (line.length > 0) {
         // strip any # symbols from the front of the line
         let frags = line.match(/^(#+ *)?(.*)/);
-        data += `\n  - ${frags[2]}`;
+        highlights.push(`\n    - ${frags[2]}`);
       }
     });
   }
 
-  // add put the result on the clipboard
-  navigator.clipboard.writeText(data).then(
+// capture the origin site info
+const originURL = new URL(pgURL.origin);
+const originHost = originURL.hostname;
+
+// TODO: Fetch API not working, so just use the origin hostname for now
+// fetchOrigin(originURL.href).then((response) => {
+//   // find the title of the origin site
+//   let originTitle = response.match(/<title>(.*?)<\/title>/)[1];
+let originTitle = 'Website at ' + originHost;
+if (siteName != '') {
+  originTitle = siteName + ' (' + originHost + ')';
+  fields.push('\n  - og:Site:: ' + siteName);
+}
+// generate the basic tana-paste entry for the origin site
+tanaPasteData = `%%tana%%\n- ${originTitle} #website`;
+tanaPasteData += `\n  - Title:: ${originTitle}`;
+tanaPasteData += `\n  - URL:: ${originURL.href}`;
+tanaPasteData += `\n  - Hostname:: ${originHost}`;
+
+console.log(tanaPasteData);
+
+if (!is_root) {
+  // if clipped page is not the root page...
+  // add tana reference/link to origin site
+  const originRef = `[[${originTitle}]]`;
+  fields.push(`\n  - Part Of Website:: ${originRef}`);
+
+  // attempt to find filename and extension
+  const pgFilename = pgPath.split('/').pop();
+  const justFileName = pgFilename.split('.')[0];
+  const pgExtension = pgFilename.split('.')[1];
+
+  // if extension is not html, then we are dealing with a file
+  if (pgExtension != undefined && pgExtension !== 'html') {
+    supertag = 'document';
+    fields.push(`\n  - Filename:: ${pgFilename}`);
+
+    if (pgTitle === '') {
+      // if pgTitle is empty, prettify the filename
+      pgTitle = titleCase(cleanFileName(justFileName));
+    }
+  }
+  fields.push(`\n  - Title:: ${pgTitle}`);
+  fields.push(`\n  - URL:: ${pgURL}`);
+
+  // generate the basic tana-paste format for the clipped page
+  // (no %%tana%%—it will be appended to original tana-paste entry)
+  const nodeData = `- ${pgTitle} #${supertag}`;
+  tanaPasteData += '\n\n\n' + nodeData;
+}
+// add the fields
+fields.forEach((field) => {
+  // console.log(field);
+  tanaPasteData += field;
+});
+tanaPasteData += highlights.join('');
+console.log(tanaPasteData);
+
+  // copy the result to the clipboard
+  navigator.clipboard.writeText(tanaPasteData).then(
     function () {
       console.log("Successfully copied data to clipboard");
     },
